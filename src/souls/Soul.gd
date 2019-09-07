18f15ee9f05351f9cbf0a7335a9db00344fe2f5a
Brett Haines <bhaines3@gatech.edu>
2019-09-06 23:29:15 -0400

    Tweaking soul area and adding SpriteFrames var

--- src/souls/Soul.gd
+++ src/souls/Soul.gd
@@ -4,11 +4,23 @@ class_name Soul
 signal upgrade_gained
 
 export (String, "double_jump", "jump_speed", "move_speed", "attack_speed", "attack_aoe", "shield_aoe", "dash") var upgrade
+export (SpriteFrames) var frames
 
-onready var area = $Area2D
 onready var sprite = $AnimatedSprite
+
+func _ready():
+	
+	# Set sprite to correct image
+	sprite.frames = frames
+	sprite.animation = "idle"
+	sprite.playing = true
 
 # Function triggered when another body enters this area
 func _on_body_entered(body):
 	if body is Player:
 		emit_signal("upgrade_gained", upgrade)
+		print("Collected soul!")
+		
+		# Remove this soul from level
+		visible = false
+		queue_free()
