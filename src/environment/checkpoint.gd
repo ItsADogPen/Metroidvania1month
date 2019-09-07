ea6f9f6d2c6661e249de7272d3633c15d3fc9776
Brett Haines <bhaines3@gatech.edu>
2019-09-07 15:23:37 -0400

    Setting checkpoint animation on body enter

--- src/environment/checkpoint.gd
+++ src/environment/checkpoint.gd
@@ -3,20 +3,21 @@ extends Area2D
 onready var area = $Area2D
 onready var sprite = $AnimatedSprite
 
-signal reached(checkpoint)
-
-var _reached = true
+var _reached = false
 
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
 	
 func set_reached():
+	_reached = true
 	sprite.animation = "reached"
 
 func set_unreached():
+	_reached = false
 	sprite.animation = "unreached"
 
 # Kept like this to make the last checkpoint checked the active one later on perhaps?
 func _on_body_entered(body: PhysicsBody2D):
-	if body.is_in_group("player"):
-		emit_signal("reached", self)
+	if body is Player:
+		set_reached()
+		body.set_checkpoint(self)
