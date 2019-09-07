67dcf9ad2febcfd3978747212f28ea0e7afd8c0b
Laurent Van Acker <laurent@pozyx.io>
2019-09-07 13:38:22 +0200

    Added souls for upgrades, moved HUD souls

--- src/souls/Soul.gd
+++ src/souls/Soul.gd
@@ -1,23 +1,27 @@
+tool
 extends Node2D
 class_name Soul
 
 export (String, "double_jump", "jump_speed", "move_speed", "attack_speed", "attack_aoe", "shield_aoe", "dash") var upgrade
-export (SpriteFrames) var frames
 
 onready var sprite = $AnimatedSprite
 
+var taken = false
+
+
 func _ready():
-	
 	# Set sprite to correct image
-	sprite.frames = frames
 	sprite.animation = "idle"
 	sprite.playing = true
 
 # Function triggered when another body enters this area
 func _on_body_entered(body):
 	if body is Player:
-		body.unlock_upgrade(upgrade)
-		
-		# Remove this soul from level
-		visible = false
-		queue_free()
+		if not taken:
+			taken = true
+			body.unlock_upgrade(upgrade)
+			
+			# Remove this soul from level
+			sprite.animation = "disappear"
+			yield(sprite, "animation_finished")
+			queue_free()
