fccb6f6502860bb7fd1dcbbf0819d4f0e2ef6cdb
Laurent Van Acker <laurent@pozyx.io>
2019-09-09 20:36:18 +0200

    Replaced Room with TestRoom

--- src/areas/room.gd
+++ src/areas/room.gd
@@ -2,11 +2,13 @@ extends Node
 class_name BaseRoom
 
 onready var projectiles = $Projectiles
+onready var audio_zones = $AudioZones
 
-var default_track : String = "res://assets/audio/music/The_Tale_of_The_Ferry_of_The_Death_arrangement.ogg"
+export(String, FILE, "*.ogg") var default_track : String = "res://assets/audio/music/The_Tale_of_The_Ferry_of_The_Death_arrangement.ogg"
 
 func _ready():
-	
+	for zone in audio_zones.get_children():
+		zone.connect("body_exited", self, "_on_body_exited")
 	AudioEngine.play_background_music(default_track)
 #	AudioEngine.play_sound("res://assets/audio/music/Pre_Boss_mixed.wav", true)
 	
@@ -16,8 +18,5 @@ func _ready():
 
 
 func _on_body_exited(body):
-	
 	AudioEngine.play_background_music(default_track)
-	
-	
 	pass
