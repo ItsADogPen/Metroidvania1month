8083acc2a0d6deca4d0bff3753ecafde791c20e0
Brett Haines <bhaines3@gatech.edu>
2019-09-13 06:38:28 -0400

    Adding barricades to room

--- src/environment/barricade.gd
+++ src/environment/barricade.gd
@@ -1,12 +1,18 @@
 extends StaticBody2D
 
+export (bool) var start_open = false
+
 onready var sprite = $AnimatedSprite
 onready var shape = $CollisionShape2D
 
+func _ready():
+	if start_open:
+		open_door()
+
 func open_door():
 	sprite.play("open")
-	shape.visible = false
+	shape.set_disabled(true)
 
 func close_door():
 	sprite.play("close")
-	shape.visible = true
+	shape.set_disabled(false)
