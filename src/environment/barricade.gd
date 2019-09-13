e6b473b92e2acd564c6a805c11a8c5820b04189a
Brett Haines <bhaines3@gatech.edu>
2019-09-13 07:42:17 -0400

    Adding area to trigger barricade close

--- src/environment/barricade.gd
+++ src/environment/barricade.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 export (bool) var start_open = false
+export (bool) var trigger_close = false
 
 onready var sprite = $AnimatedSprite
 onready var shape = $CollisionShape2D
@@ -8,6 +9,10 @@ onready var shape = $CollisionShape2D
 func _ready():
 	if start_open:
 		open_door()
+	
+	# Only activate trigger zone on the boss barricade
+	if trigger_close:
+		$Area2D.connect("body_entered", self, "_on_body_entered")
 
 func open_door():
 	sprite.play("open")
@@ -16,3 +21,7 @@ func open_door():
 func close_door():
 	sprite.play("close")
 	shape.set_disabled(false)
+
+func _on_body_entered(body):
+	if body is Player:
+		close_door()
